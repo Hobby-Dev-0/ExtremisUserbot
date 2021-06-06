48455f007ecc40e83aa094e88ebb30750cad1fb1
Aman Pandey <72003733+amanpandey7647@users.noreply.github.com>
2021-06-06 16:22:00 +0530

    OK

---
+++ test/__init__.py
@@ -0,0 +1,9 @@
+# pylint: disable=missing-module-docstring
+#
+# Copyright (C) 2020 by https://github.com/amanpandey7647/ExtremisUserbot@Github, < https://github.com/amanpandey7647/ExtremisUserbot >.
+#
+# This file is part of < https://github.com/amanpandey7647/ExtremisUserbot > project,
+# and is released under the "GNU v3.0 License Agreement".
+# Please see < https://github.com/amanpandey7647/ExtremisUserbot >
+#
+# All rights reserved.
