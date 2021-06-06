8d7e72ef155720fafbcf11feddf41cabffdaad91
Aman Pandey <72003733+amanpandey7647@users.noreply.github.com>
2021-06-06 16:50:28 +0530

    Extremis

---
+++ extremis/__init__.py
@@ -0,0 +1,8 @@
+# All rights reserved.
+
+from extremis.logger import logging  # noqa
+from extremis.config import Config, get_version  # noqa
+from extremis.core import (  # noqa
+    Extremis, filters, Message, get_collection, pool)
+
+extremis = Extremis()  #
